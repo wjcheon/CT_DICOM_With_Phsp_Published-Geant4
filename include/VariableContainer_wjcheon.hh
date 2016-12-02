c07d28606565def5708c1312bbb13554ea7cf815
wjcheon <wonjoongcheon@gmail.com>
2016-12-03 05:24:59 +0900

    modify uploading

--- include/VariableContainer_wjcheon.hh
+++
@@ -1,8 +0,0 @@
-//
-// 	Author: wjcheon (Medical physics Lab, Sungkyunkwan University, Seoul, Republic of Korea)
-//  GitHub: http://github.com/wjcheon
-//
-
-
-#define REPLICA_NUM  100
-#define NUMBER_OF_PARTICLE_RAM 10
